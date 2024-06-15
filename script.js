c14496c687ec13d214fbba0e48cecf695f595c04
JuravskiLiron <juravskliron@gmail.com>
2024-06-15 18:44:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const stars = document.querySelectorAll(".star");
+const emojiEl = document.querySelector(".emoji");
+const statusEl = document.querySelector(".status");
+const defaultRatingIndex = 0;
+let currentRatingIndex = 0;
+
+const ratings = [
+  { emoji: "", name: "Поставь оценку" },
+  { emoji: "😔", name: "Очень плохо" },
+  { emoji: "🙁", name: "Плохо" },
+  { emoji: "🙂", name: "Хорошо" },
+  { emoji: "🤩", name: "Очень хорошо" },
+  { emoji: "🥰", name: "Отлично" }
+];
+
+const checkSelectedStar = (star) => {
+  if (parseInt(star.getAttribute("data-rate")) === currentRatingIndex) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const setRating = (index) => {
+  stars.forEach((star) => star.classList.remove("selected"));
+  if (index > 0 && index <= stars.length) {
+    document
+      .querySelector('[data-rate="' + index + '"]')
+      .classList.add("selected");
+  }
+  emojiEl.innerHTML = ratings[index].emoji;
+  statusEl.innerHTML = ratings[index].name;
+};
+
+const resetRating = () => {
+  currentRatingIndex = defaultRatingIndex;
+  setRating(defaultRatingIndex);
+};
+
+stars.forEach((star) => {
+  star.addEventListener("click", function () {
+    if (checkSelectedStar(star)) {
+      resetRating();
+      return;
+    }
+    const index = parseInt(star.getAttribute("data-rate"));
+    currentRatingIndex = index;
+    setRating(index);
+  });
+
+  star.addEventListener("mouseover", function () {
+    const index = parseInt(star.getAttribute("data-rate"));
+    setRating(index);
+  });
+
+  star.addEventListener("mouseout", function () {
+    setRating(currentRatingIndex);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  setRating(defaultRatingIndex);
+});
